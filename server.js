80433e2704355f68190499a167282dfad7f0ebd3
Ansh-Sonkusare <59761933+Ansh-Sonkusare@users.noreply.github.com>
2021-08-24 13:05:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,4 @@ io.on('connection', socket => {
   })
 })
 const port = process.env.PORT ||  3000
-server.listen(port, () => {console.log(`success on port ${port}`)})
+server.listen(port, '0.0.0.0', () => {console.log(`success on port ${port}`)})
